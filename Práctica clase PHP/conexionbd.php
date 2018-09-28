f9b7f23934f7cffe99de02b7fdbb04272f66c30d
Katherine2000 <katy200021@gmail.com>
2018-09-28 11:03:46 -0500

    Add files via upload

---
+++ Práctica clase PHP/conexionbd.php
@@ -0,0 +1,8 @@
+<?php
+
+	$mysqli = new mysqli('localhost','root','','mi_revista');
+	$mysqli -> set_charset("utf8");
+	$query = $mysqli -> query("SELECT * FROM ciudad");
+	$query2 = $mysqli -> query("SELECT * FROM revista");
+?>
+
